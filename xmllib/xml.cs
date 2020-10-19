152176dc096005a848b83aeeaf849932d1c66e49
Greg Doud <gpdoud@gmail.com>
2020-10-19 10:09:25 -0400

    "final commit"

--- xmllib/xml.cs
+++ xmllib/xml.cs
@@ -3,19 +3,31 @@ using System.Xml;
 
 namespace DSI.Xlmlib {
 
+    public enum CategoryTypes { NULL, VAR, ARGUMENT, STATIC, FIELD, CLASS, SUBROUTINE };
+
     public class Xml { 
 
         string filename = null;
         XmlDocument doc = null;
 
-        public void Add(string token, string tokenType) {
+        public void Add(string token, string tokenType, CategoryTypes category = CategoryTypes.NULL, int index = -1) {
             var element = doc.CreateElement("Token");
             var dataAttr = doc.CreateAttribute("Value");
+            var catAttr = doc.CreateAttribute("Category");
+            var idxAttr = doc.CreateAttribute("Index");
             dataAttr.InnerText = token;
             var typeAttr = doc.CreateAttribute("Type");
             typeAttr.InnerText = tokenType;
             element.Attributes.Append(typeAttr);
             element.Attributes.Append(dataAttr);
+            if(category != CategoryTypes.NULL) {
+                catAttr.InnerText = category.ToString();
+                element.Attributes.Append(catAttr);
+            }
+            if(index != -1) {
+                idxAttr.InnerText = index.ToString();
+                element.Attributes.Append(idxAttr);
+            }
             doc.DocumentElement.AppendChild(element);
         }
 
